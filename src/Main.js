75d7d7b5f359645feee8891ff1aa308b22e74bfc
queen-x-vee <valentinabuoro4@gmail.com>
2023-04-11 19:39:33 +0100

    second commit

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const Main = () => {
+  const country = 'Nigeria'
   return (
     <>
     <main>
      <section>
        <h2>About Us</h2>
        <p>
-         We are a small team of web developers based in the United States. We
+         We are a small team of web developers based in the {country}. We
          specialise in creating modern and responsive websites for businesses
          and individuals.
        </p>
